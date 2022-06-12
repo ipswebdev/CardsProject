da792d9e9f0ccfcf7ff8c62812d435957cfcb7cc
pranay sawant <sawant.pranay.97@gmail.com>
2022-06-13 00:09:09 +0530

    added dumb component

--- src/app/cards/card/card.component.ts
+++ src/app/cards/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() card;
+  @Input() index;
+  @Output() removeCard = new EventEmitter; 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  removeCardHandler(index){
+    this.removeCard.emit(index);
   }
 
 }
